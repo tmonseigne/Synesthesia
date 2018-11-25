8a2eced385f85cb93116f8b8659e11fe132d7359
Rack <rack.kendorim@gmail.com>
2018-11-25 17:26:45 +0100

    :art: Little format code

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,13 +1,13 @@
 #include <QApplication>
 #include <QPushButton>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    QApplication app(argc, argv);
+	QApplication app(argc, argv);
 
-    QPushButton hello("Hello world!");
-    hello.resize(200, 60);
+	QPushButton hello("Hello world!");
+	hello.resize(200, 60);
 
-    hello.show();
-    return app.exec();
+	hello.show();
+	return app.exec();
 }
--- src/sound-processing/main.cpp
+++ src/sound-processing/main.cpp
@@ -1,8 +1,8 @@
-
 #include <conio.h>
 #include <iostream>
 
 using namespace std;
+
 int main()
 {
 	cout << "Hello CMake." << endl;
